cb79b1a3da32328541fd6c38e3b3153941d2bda9
HawkNguyen <44883228+HawkNguyen@users.noreply.github.com>
2018-11-26 00:02:14 -0500

    Update BusinessList.js for 2nd part

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,12 +5,9 @@ class BusinessList extends React.Component{
   render(){
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {
+          this.props.businesses.map(e => <Business business={e}/>)
+        }
       </div>
     );
   };
